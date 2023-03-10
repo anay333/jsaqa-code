51d87eabb7c5379cd1b10494cba4aeb5eb7ec837
anay333 <87089288+anay333@users.noreply.github.com>
2023-03-10 22:22:44 +0500

    Update sortByName.test.js

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,15 +2,18 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    const input = [
+  const input = [
         "Гарри Поттер",
-        "Гарри Поттер",
-        "Гарри Поттер"
+        "Властелин Колец",
+        "Волшебник изумрудного города"
     ]
 
-    const expected = input
+    const expected = [
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+        "Гарри Поттер"
+    ]
     const result = sorting.sortByName(input)
     expect(result).toEqual(expected);
   });
 });
-
