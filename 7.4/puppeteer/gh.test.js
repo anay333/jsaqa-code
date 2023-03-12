dd2703cadbd553aee09a794053af19214780d7ee
anay333 <87089288+anay333@users.noreply.github.com>
2023-03-12 22:42:59 +0500

    tests added

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,22 +1,23 @@
 let page;
-
-beforeEach(async () => {
-   page = await browser.newPage();
-  await page.goto("https://github.com/team");
-  jest.setTimeout(100000)
-});
+jest.setTimeout(100000);
 
 afterEach(() => {
   page.close();
 });
 
 describe("Github page tests", () => {
+  
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.goto("https://github.com/team");
+});
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Let’s build from here · GitHub');
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
   });
 
   test("The first link attribute", async () => {
@@ -34,5 +35,31 @@ describe("Github page tests", () => {
   });
 });
 
+describe("Github next page tests", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/account");
+  });
+   test("The h1 header content'", async () => {
+    const firsSteptLink = await page.$("#login > div.auth-form-header.p-0 > h1");
+    await page.waitForSelector('h1');
+    const title3 = await page.title();
+    expect(title3).toEqual('Sign in to GitHub · GitHub');
+  });
+
+  test("The first step link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href'));
+    expect(actual).toEqual("#start-of-content");
+  });
+
+  test("The page contains another Sign in button", async () => {
+    const btnSelector = "#login > p";
+    await page.waitForSelector(btnSelector, { 
+      visible: true
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("New to GitHub?")
+  });
+});
 
 
