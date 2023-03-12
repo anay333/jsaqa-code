868608b83cf950e016a5e2d507dbb4a6f6500dfc
anay333 <87089288+anay333@users.noreply.github.com>
2023-03-12 19:15:38 +0500

    timeout added

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,7 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
+  await page.goto("https://github.com/team", {timeout: 1000});
 });
 
 afterEach(() => {
@@ -13,19 +13,20 @@ describe("Github page tests", () => {
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector('h1');
+    await page.waitForSelector('h1', {timeout: 1000});
     const title2 = await page.title();
     expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
   });
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a", link => link.getAttribute('href'), {timeout: 1000});
     expect(actual).toEqual("#start-of-content");
   });
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
-    await page.waitForSelector(btnSelector, {
+    await page.waitForSelector(btnSelector, { 
+      timeout: 1000,
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
