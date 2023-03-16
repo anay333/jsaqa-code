2296b4261bea07784a421c62618b60af5f2b94a7
anay333 <87089288+anay333@users.noreply.github.com>
2023-03-17 01:55:04 +0500

    Update net.test.js

--- 7.5/puppeteer-2/net.test.js
+++ 7.5/puppeteer-2/net.test.js
@@ -1,53 +1,44 @@
 const { expect, config } = require("chai");
+const { FormatterBuilder } = require("cucumber");
 const { clickElement, getText} = require("./lib/commands.js");
 
 let page;
 
 beforeEach(async () => {
-  page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0);
+    page = await browser.newPage();
+    await page.goto("http://qamid.tmweb.ru/client/index.php");
 });
 
 afterEach(() => {
-  page.close();
+    page.close();
 });
 
-describe("Cinema tickets tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("http://qamid.tmweb.ru/client/index.php");
-  });
-
-  test("The first test'", async () => {
-    const title = await page.title();
-    console.log("Page title: " + title);
-    await clickElement(page,"body > nav > a.page-nav__day.page-nav__day_chosen > span.page-nav__day-number");
-  });
-  
-  test ("The first link leads on booking page", async () => {
-    await clickElement(page,"body > nav > a:nth-child(7)");
-    await clickElement(page,"body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(1) > a");
-    const actual = await getText(page, "body > main > section > button");
-    await expect(actual).toContain("Забронировать");
-  });
-
-  test ("assert that a nav exist", async () => {
-    const navbar = await page.$eval(".page-nav", el => (el ? true : false));
-    expect(navbar).toBe(true);
  
+test("Should book one ticket", async () => {
+  await clickElement(page, "a:nth-child(2) > span.page-nav__day-week"); 
+  await clickElement(page, "a.movie-seances__time");  
+  await clickElement(page, ".buying-scheme__chair_standart:not(.buying-scheme__chair_taken)"); 
+  await clickElement(page, "button.acceptin-button"); 
+  const actual = await getText(page, "h2.ticket__check-title");
+  expect(actual).contain("Вы выбрали билеты:");
 });
 
-test("assert that main title contains the correct text", async () => {
-  const mainTitleText = await page.$eval("body > header > h1", el => el.textContent);
-  expect(mainTitleText).toEqual("Идёмвкино");
+test("Should buy two tickets", async () => {
+  await clickElement(page, "a:nth-child(2) > span.page-nav__day-week"); 
+  await clickElement(page, "a.movie-seances__time");  
+  await clickElement(page, ".buying-scheme__chair_standart:not(.buying-scheme__chair_taken)"); 
+  await clickElement(page, "button.acceptin-button"); 
+  const actual = await getText(page, "h2.ticket__check-title");
+  expect(actual).contain("Вы выбрали билеты:");
 });
 
-test.only("can't click the button", async () => {
-  await clickElement(page,"body > nav > a:nth-child(4)");
-  await clickElement(page,"body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(3) > a");
-  pageNext = await browser.newPage();
-  await pageNext.goto("https://qamid.tmweb.ru/client/hall.php");
-  const result = await clickElement(pageNext,"body > main > section > button");
- expect(result).equal(undefined);
-})
-})
+
+test("Should not be able to book taken chair", async () => {
+      await clickElement(page,"body > nav > a:nth-child(3) > span.page-nav__day-number");
+      await clickElement(page,"body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(2) > a");
+      pageNext = await browser.newPage();
+      await pageNext.goto("https://qamid.tmweb.ru/client/hall.php");
+      await clickElement(page,"body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(4) > span.buying-scheme__chair.buying-scheme__chair_vip.buying-scheme__chair_taken");
+      const actual = await page.$eval(".acceptin-button", (link) => link.getAttribute("disabled"));
+      expect(actual).equal("true");
+});
